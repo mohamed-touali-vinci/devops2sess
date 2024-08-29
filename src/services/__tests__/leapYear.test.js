6e5f8924ffe8183ee336ccf3be4a1b9eeb83e26c
Mohamed Touali <mohamed.touali@student.vinci.com>
2024-08-29 17:05:48 +0200

    Ajout des tests

---
+++ src/services/__tests__/leapYear.test.js
@@ -0,0 +1,17 @@
+const isLeapYear = require('../isLeapYear');
+
+test('2000 est une année bissextile', () => {
+  expect(isLeapYear(2000)).toBe(true);
+});
+
+test('1700 n\'est pas une année bissextile', () => {
+  expect(isLeapYear(1700)).toBe(false);
+});
+
+test('2008 est une année bissextile', () => {
+  expect(isLeapYear(2008)).toBe(true);
+});
+
+test('2017 n\'est pas une année bissextile', () => {
+  expect(isLeapYear(2017)).toBe(false);
+});
